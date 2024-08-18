b4a1ae9a9a9ace03e43a5d1ecacf3f51d01ecbd2
anuragunnikannan <anuragunnikannan7@gmail.com>
2024-08-18 22:25:07 +0530

    updated script

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@
 
 sshpass -p password scp -o StrictHostKeyChecking=no /app/input.sql anurag@ip:/home/anurag/
 
-command="echo password | sudo docker cp /home/anurag/input.sql pg-container:/home/ > /dev/null 2>/dev/null && echo password | sudo -S docker exec -i pg-container psql -d postgres < input.sql > /home/anurag/output.txt 2>/dev/null && cat output.txt"
+command="echo password | sudo docker cp /home/anurag/input.sql pg-container:/home/ > /dev/null 2>/dev/null && echo password | sudo -S docker exec -i pg-container psql -U postgres -d postgres < input.sql > /home/anurag/output.txt 2>/dev/null && cat output.txt"
 
 output=`sshpass -p password ssh -o StrictHostKeyChecking=no -t anurag@ip ${command}`
 
