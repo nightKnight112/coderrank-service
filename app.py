3622141f4a2da61194c28e626f871acd0f93d169
anuragunnikannan <anuragunnikannan7@gmail.com>
2024-09-13 02:27:12 +0530

    removed snippet for code execution from api

--- app.py
+++ app.py
@@ -15,29 +15,6 @@ language_options = [
     }
 ]
 
-def execute_java_code(code, input):
-    with open("./codes/Solution.java", "w") as f:
-        f.write(code)
-    
-    with open("./codes/input.txt", "w") as f:
-        f.write(input)
-
-    output = subprocess.run(["./script.sh"], capture_output=True, text=True)
-    print("Output: ",output.stdout)
-    print("Error: ", output.stderr)
-    if(len(output.stderr) > len(output.stdout)):
-        return output.stderr
-    else:
-        return output.stdout
-    
-
-def execute_query(query):
-    with open("input.sql", "w") as f:
-        f.write(query)
-    
-    output = subprocess.run(["./script.sh"], capture_output=True, text=True)
-    print(output.stdout)
-
 @app.route('/execute', methods=['POST'])
 def execute():
     data = request.get_json()
@@ -71,72 +48,6 @@ def execute():
     
     response = response.strip()
     return jsonify(response)
-
-@app.route('/execute_code_docker', methods=['POST'])
-def execute_code():
-    data = request.json
-    container_id = "239d386b5caa8fd0154b0830f31571c14acb024d240b23aa59a2ee980e272ff4"
-    code = data["code"]
-    language = data["language"].lower()
-    user_input = data.get("input", "")
-
-    if not container_id or not language or not code:
-        return jsonify({"error": "language, code, and input are required"}), 400
-
-    try:
-        container = client.containers.get(container_id)
-
-        if language == 'python':
-            local_file_path = "./codes/app.py"
-            container_file_path = "/app.py"
-
-            with open(local_file_path, "w") as f:
-                f.write(code)
-            with open("./codes/input.txt", "w") as f:
-                f.write(user_input)
-            
-            subprocess.run(['docker', 'cp', local_file_path, f'{container_id}:{container_file_path}'], check=True)
-            subprocess.run(['docker', 'cp', "./codes/input.txt", f'{container_id}:{"/input.txt"}'], check=True)
-
-            exec_cmd = f'python app.py < input.txt'
-            result = container.exec_run(
-                ['sh', '-c', exec_cmd],
-                stdout=True,
-                stderr=True
-            )
-            return jsonify({"output": result.output.decode('utf-8')}), 200
-
-        elif language == 'java':
-            # Write Java code to a file on the host machine containing compilers
-            local_file_path = "./codes/Solution.java"
-            container_file_path = "/Solution.java"
-            with open(local_file_path, "w") as f:
-                f.write(code)
-            with open("./codes/input.txt", "w") as f:
-                f.write(user_input)
-
-            # Copy the Java file to the container on the host machine containing compilers
-            subprocess.run(['docker', 'cp', local_file_path, f'{container_id}:{container_file_path}'], check=True)
-            subprocess.run(['docker', 'cp', "./codes/input.txt", f'{container_id}:{"/input.txt"}'], check=True)
-
-            # Compile the Java file inside the container and pass the input
-            exec_cmd = f'javac Solution.java && java Solution < input.txt'
-            result = container.exec_run(['sh', '-c', exec_cmd], stdin=True, stdout=True, stderr=True)
-            # if compile_result.exit_code != 0:
-            #     return jsonify({"error": compile_result.output.decode('utf-8')}), 500
-
-            # # Run the compiled Java program with input piped directly
-            # command = f'echo "{user_input}" | java Main'
-            # result = container.exec_run(['sh', '-c', command], stdin=True, tty=True)
-
-            return jsonify({"output": result.output.decode('utf-8')}), 200
-
-        else:
-            return jsonify({"error": "Unsupported language"}), 400
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
 
 #api to fetch all supported languages, language_id param to be used as peimary key for testcases
 @app.route('/get-language-options', methods=["GET"])
