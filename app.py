38f0b6ce4f10ee019201fbcc50e9bb4a3ede580c
Archisman Das <archisman112358@gmail.com>
2024-12-28 14:48:21 +0530

    added logs to visualize http object

--- app.py
+++ app.py
@@ -106,54 +106,54 @@ def execute(language_name, code, input, user_uuid):
     
     return output.json()
 
-@app.post("/execute")
-def execute():
-    logging.debug(f"{request.method} - {request.url} - {request.json}")
-    try:
-        data = request.json
-        code = data["code"]
-        language_name = data["language_name"]
-        user_uuid = data["user_uuid"]
-        input = data["input"]
-        file = ""
-        stdout = ""
-        stderr = ""
+# @app.post("/execute")
+# def execute():
+#     logging.debug(f"{request.method} - {request.url} - {request.json}")
+#     try:
+#         data = request.json
+#         code = data["code"]
+#         language_name = data["language_name"]
+#         user_uuid = data["user_uuid"]
+#         input = data["input"]
+#         file = ""
+#         stdout = ""
+#         stderr = ""
 
-        filename = ""
+#         filename = ""
 
-        os.makedirs(os.path.dirname(f"/home/codes/{user_uuid}/"), exist_ok=True)
-        with open(f"/home/codes/{user_uuid}/input.txt", "w") as f:
-            f.write(input)
+#         os.makedirs(os.path.dirname(f"/home/codes/{user_uuid}/"), exist_ok=True)
+#         with open(f"/home/codes/{user_uuid}/input.txt", "w") as f:
+#             f.write(input)
 
-        if language_name == "Java":
-            filename = f"/home/codes/{user_uuid}/Solution.java"
-            with open(filename, "w") as f:
-                f.write(code)
-        elif language_name == "Python":
-            filename = f"/home/codes/{user_uuid}/solution.py"
-            with open(filename, "w") as f:
-                f.write(code)
+#         if language_name == "Java":
+#             filename = f"/home/codes/{user_uuid}/Solution.java"
+#             with open(filename, "w") as f:
+#                 f.write(code)
+#         elif language_name == "Python":
+#             filename = f"/home/codes/{user_uuid}/solution.py"
+#             with open(filename, "w") as f:
+#                 f.write(code)
         
-        with open(f"/home/codes/{user_uuid}/input.txt", "rb") as f:
-            file = f.read()
+#         with open(f"/home/codes/{user_uuid}/input.txt", "rb") as f:
+#             file = f.read()
 
-        if language_name == "Java":
-            output = subprocess.run(["java", filename], input=file, capture_output=True)
-            stdout = output.stdout.decode().strip()
-            stderr = output.stderr.decode().strip()
-        else:
-            output = subprocess.run(["python3", filename], input=file, capture_output=True)
-            stdout = output.stdout.decode().strip()
-            stderr = output.stderr.decode().strip()
+#         if language_name == "Java":
+#             output = subprocess.run(["java", filename], input=file, capture_output=True)
+#             stdout = output.stdout.decode().strip()
+#             stderr = output.stderr.decode().strip()
+#         else:
+#             output = subprocess.run(["python3", filename], input=file, capture_output=True)
+#             stdout = output.stdout.decode().strip()
+#             stderr = output.stderr.decode().strip()
 
-        if len(stderr) > len(stdout):
-            return jsonify({"output": stderr})
-        else:
-            return jsonify({"output": stdout})
+#         if len(stderr) > len(stdout):
+#             return jsonify({"output": stderr})
+#         else:
+#             return jsonify({"output": stdout})
         
-    except Exception as e:
-        logging.error(e)
-        return jsonify({"output": "something went wrong"}), 500
+#     except Exception as e:
+#         logging.error(e)
+#         return jsonify({"output": "something went wrong"}), 500
 
 @app.route("/run-code", methods=["POST"])
 def run_code():
